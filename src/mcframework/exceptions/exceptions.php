c1e534eb9b699273e0a2e6a764fb18fc828924c4
chenzhe <chenzhe@meicai.cn>
2017-12-27 21:46:43 +0800

    first commit

---
+++ src/mcframework/exceptions/exceptions.php
@@ -0,0 +1,9 @@
+<?php
+
+ namespace mcframework\exceptions;
+
+class Excepttt {
+ public function test2() {
+  echo "test2sadas2";
+ }
+ }
---
+++ src/mcframework/utils/util.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace mcframework\utils;
+
+class Util {
+  public function test1() {
+   echo "dsadasdsa\n\n";
+}
+}
