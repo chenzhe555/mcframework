1301a2e2d4bfb3984eca905ad653a7cb9111a49d
chenzhe <chenzhe@meicai.cn>
2017-12-28 23:32:29 +0800

    add httputil

---
+++ src/MCUtils/MCHttpUtil.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 陈哲是个好孩子
+ * Date: 2017/12/28
+ */
+
+namespace mcframework\mcutils;
+
+class MCHttpUtil {
+
+    /*
+     * 构建网络请求成功结果
+     * */
+    public static function buildSuccessResult($param = []) {
+        return [
+            "ret" => 1,
+            "data" => $param
+        ];
+    }
+
+    /*
+     * 构建网络请求失败结果
+     * */
+    public static function buildFailResult($msg = "",$code = 0) {
+        return [
+            "ret" => 0,
+            "error" => [
+                "errorMsg" => $msg,
+                "errorCode" => $code
+            ]
+        ];
+    }
+}
