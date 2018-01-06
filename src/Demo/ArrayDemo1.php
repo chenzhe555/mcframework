15cedb21ff813935da02d02d82478f2c55d10bfe
chenzhe <chenzhe@meicai.cn>
2018-01-06 17:00:31 +0800

    提交测试代码

---
+++ src/Demo/ArrayDemo1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 陈哲是个好孩子
+ * Date: 2018/1/5
+ */
+
+namespace mcframework\demo;
+
+class ArrayDemo1 {
+    /*
+     * 1.array_multisort — 对多个数组或多维数组进行排序
+     * */
+
+    public static function test()
+    {
+        echo "123\n";exit();
+    }
+}
+
+ArrayDemo1::test();
